9605271cbdd5e39803c2225c08e0ea60a359a7ee
Yusuf Khan <Yusuf Khan>
2023-09-08 11:18:45 -0700

    reg test

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
@@ -7,12 +8,10 @@ from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 class bdist_wheel(_bdist_wheel):
     def finalize_options(self):
         _bdist_wheel.finalize_options(self)
-        # Mark us as not a pure python package
         self.root_is_pure = False
 
     def get_tag(self):
         python, abi, plat = _bdist_wheel.get_tag(self)
-        # We don't contain any python source
         python, abi = 'py2.py3', 'none'
         return python, abi, plat
 
@@ -20,18 +19,19 @@ class bdist_wheel(_bdist_wheel):
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
+        print("Running post-install script...")
         install.run(self)
-        # RUN CUSTOM POST-INSTALL SCRIPT HERE
         setup_dir = os.path.dirname(os.path.realpath(__file__))
         post_install_script = os.path.join(setup_dir, 'setup', 'post_install.py')
-        os.system(f"python {post_install_script}")
+        print(f"Running {post_install_script}...")
+        os.system(f"{sys.executable} {post_install_script}")
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name="azpype",
-    version="0.3.9",
+    version="0.3.10",
     description="A native Python interface wrapping AzCopy for bulk data transfer to and from Azure Blob Storage.",
     long_description=open('README.md', encoding="UTF-8").read(),
     long_description_content_type='text/markdown',
